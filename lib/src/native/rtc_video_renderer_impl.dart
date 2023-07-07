4cc7df2cba22553e1bd967388c730a62e7e2d4de
n.morozov <n.morozov@teleguard.com>
2023-07-07 15:55:51 +0300

    renderer setSrcObject() method returns Future

--- lib/src/native/rtc_video_renderer_impl.dart
+++ lib/src/native/rtc_video_renderer_impl.dart
@@ -61,11 +61,11 @@ class RTCVideoRenderer extends ValueNotifier<RTCVideoValue>
     });
   }
 
-  void setSrcObject({MediaStream? stream, String? trackId}) {
+  Future<void> setSrcObject({MediaStream? stream, String? trackId}) {
     if (textureId == null) throw 'Call initialize before setting the stream';
 
     _srcObject = stream;
-    WebRTC.invokeMethod('videoRendererSetSrcObject', <String, dynamic>{
+    return WebRTC.invokeMethod('videoRendererSetSrcObject', <String, dynamic>{
       'textureId': textureId,
       'streamId': stream?.id ?? '',
       'ownerTag': stream?.ownerTag ?? '',
